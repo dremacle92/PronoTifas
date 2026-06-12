b856273ec7dbd44487acf0680bc7ede5b7b0e25a
dremacle <dremacle@thelis.be>
2026-06-12 11:21:23 +0200

    misc

--- config.js
+++ config.js
@@ -6,5 +6,6 @@
 //    → ton projet → Settings → API
 const SUPABASE_URL  = 'https://rraoxfbfwkgwbatnsxmn.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_t_-025V2GTtrk_83da3EOw_j-Y4sw1Z';
+const FOOTBALL_API_KEY = '61809510a4e348ec948fe9318d742831';
 
 const ADMIN_PASSWORD = 'PronoTifas2026';
